0c2b452e1e17785ef2180e13bc0e12c5d0dd5673
tronghoa <hoa.truongtrong26@gmail.com>
2024-06-13 15:37:57 +0700

    feat(10): store changed

---
+++ src/AssetManagement.Application/Models/Requests/AssetRequest.cs
@@ -0,0 +1,20 @@
+﻿using AssetManagement.Domain.Entities;
+using AssetManagement.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Application.Models.Requests
+{
+    public class AssetRequest
+    {
+        public string AssetName { get; set; } = string.Empty;
+        public Guid CategoryId { get; set; }
+        public string Specification { get; set; } = string.Empty;
+        public DateOnly InstallDate { get; set; }
+        public EnumAssetStatus Status { get; set; } = EnumAssetStatus.Available;
+    }
+}
