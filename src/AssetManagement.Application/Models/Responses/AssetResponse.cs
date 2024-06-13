d2c1ae89e3af7387332a64dd9abb5562d5da7609
Nguyễn Đắc Trung Phương <trungphuongtxn@gmail.com>
2024-06-13 23:28:54 +0700

    fix: fix api convention, file name convention

--- src/AssetManagement.Application/Models/Responses/AssetResponse.cs
+++ src/AssetManagement.Application/Models/Responses/AssetResponse.cs
@@ -5,13 +5,16 @@ using System.Collections.Generic;
 
 namespace AssetManagement.Application.Models.Responses
 {
-    public class AssetResponse 
+    public class AssetResponse
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string AssetCode { get; set; } = string.Empty;
         public string AssetName { get; set; } = string.Empty;
-        public Guid CategoryId {  get; set; }
-        public string CategoryName { get; set; }
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public DateOnly InstallDate { get; set; }
+        public string Specification { get; set; } = string.Empty;
         public EnumAssetStatus Status { get; set; } = EnumAssetStatus.Available;
+        public Guid LocationId { get; set; }
+        public string LocationName { get; set; } = string.Empty;
     }
-}
