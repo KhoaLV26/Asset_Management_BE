2ef156690555b7c250dd895f6fde6c0523a63022
tronghoa <hoa.truongtrong26@gmail.com>
2024-06-17 21:22:42 +0700

    fix(5): edit request value for Chage pass and response for user

---
+++ src/AssetManagement.Application/Models/Requests/ChangePasswordRequest.cs
@@ -0,0 +1,23 @@
+﻿using AssetManagement.Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Application.Models.Requests
+{
+    public class ChangePasswordRequest
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string OldPassword { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression(RegexConstants.PASSWORD, ErrorMessage = ErrorMessage.ERROR_PASSWORD)]
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
