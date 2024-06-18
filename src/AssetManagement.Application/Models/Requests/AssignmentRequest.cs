7aaa80cbaab5ebb285b72426216b6d252f2a227d
QuanPM <m.quan1804@gmail.com>
2024-06-18 18:31:03 +0700

    Update

--- src/AssetManagement.Application/Models/Requests/AssignmentRequest.cs
+++ src/AssetManagement.Application/Models/Requests/AssignmentRequest.cs
@@ -10,7 +10,6 @@ namespace AssetManagement.Application.Models.Requests
 {
     public class AssignmentRequest
     {
-        //public Guid Id { get; set; }
         public Guid AssignedTo { get; set; }
         public Guid AssignedBy { get; set; }
         public DateTime AssignedDate { get; set; }
