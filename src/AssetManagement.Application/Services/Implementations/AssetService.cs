2ba8c0bced628d9b66f21f847d0ba5fcddb17d7e
phucvhhy212 <phucvhhy212@gmail.com>
2024-06-24 01:18:10 +0700

    fix asset detail

--- src/AssetManagement.Application/Services/Implementations/AssetService.cs
+++ src/AssetManagement.Application/Services/Implementations/AssetService.cs
@@ -118,6 +118,9 @@ namespace AssetManagement.Application.Services.Implementations
                 AssetName = asset.AssetName,
                 AssetCode = asset.AssetCode,
                 CategoryId = asset.CategoryId,
+                CategoryName = asset.Category.Name,
+                Specification = asset.Specification,
+                InstallDate = asset.InstallDate,
                 Status = asset.Status,
                 AssignmentResponses = assignmentResponses.Select(a => new AssignmentResponse
                 {
--- src/AssetManagement.Infrastructure/Repositories/AssetRepository.cs
+++ src/AssetManagement.Infrastructure/Repositories/AssetRepository.cs
@@ -18,38 +18,16 @@ namespace AssetManagement.Infrastructure.Repositories
         {
             _context = context;
         }
-        public async Task<(IEnumerable<Asset> items, int totalCount)> GetAllAsync(int page = 1, Expression<Func<Asset, bool>>? filter = null,
-    Func<IQueryable<Asset>, IOrderedQueryable<Asset>>? orderBy = null, string includeProperties = "")
-        {
-            IQueryable<Asset> query = _context.Set<Asset>();
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-
-            foreach (var includeProperty in includeProperties.Split
-                         (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-            {
-                query = query.Include(includeProperty);
-            }
-
-            var totalCount = query.Count();
-
-            if (orderBy != null)
-            {
-                query = orderBy(query);
-            }
-
-            query = query.Skip((page - 1) * PageSizeConstant.PAGE_SIZE).Take(PageSizeConstant.PAGE_SIZE);
-
-            var items = await query.ToListAsync();
-            return (items, totalCount);
-        }
 
         public async Task<Asset?> GetAssetDetail(Guid id)
         {
-            return await _context.Assets.Include(x => x.Assignments).ThenInclude(x => x.UserBy).Include(x => x.Assignments)
-                .ThenInclude(x => x.UserTo).FirstOrDefaultAsync(x=>x.Id == id);
+            return await _context.Assets
+                .Include(x => x.Category)
+                .Include(x => x.Assignments)
+                .ThenInclude(x => x.UserBy)
+                .Include(x => x.Assignments)
+                .ThenInclude(x => x.UserTo)
+                .FirstOrDefaultAsync(x=>x.Id == id);
         }
     }
 }
