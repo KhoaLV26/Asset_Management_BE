c235360987612f0c36e4625625d99e841b9f954c
QuanPM <m.quan1804@gmail.com>
2024-07-04 14:29:28 +0700

    hotfix

---
+++ src/AssetManagement.Domain/Constants/AgeConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Domain.Constants
+{
+    public class AgeConstants
+    {
+        public const int REQUIRED_AGE = -18;
+    }
+}
